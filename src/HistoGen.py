91ba6389e38579e4c8c95cbfd921efbd4b4f1ce8
Effofex <effofex.steem@gmail.com>
2018-06-26 20:57:11 -0400

    Arranged things so that HistoGen is importing packages correctly

---
+++ src/HistoGen.py
@@ -0,0 +1,5 @@
+import SafReader as sr
+
+if __name__ == "__main__":
+    print("Hello")
+    sr.read("thisfile.saf")
--- src/SafReader.py
+++ src/SafReader.py
@@ -1,17 +1,19 @@
-class SafReader:
-    """Utility class which parses a MadAnalysis5 *.saf file describing one or 
-    more histograms and is capable of translating them to a tidy format for
+def read(fileLoc):
+    """Utility function which parses a MadAnalysis5 *.saf file describing one 
+    or more histograms and is capable of translating them to a tidy format for
     further data analysis
-    """
     
-    @classmethod
-    def read(fileLoc):
-    """Actually read the file and produce a pandas dataframe representing it in
+    Actually read the file and produce a pandas dataframe representing it in
     a Tidy, denormalized table
 
     Keyword arguments:
     fileLoc -- the path and filename of the *.saf file
     
     Returns:
-    A pandas dataframe representing all the histogram data
+    A pandas dataframe representing all the histogram data in a tidy,
+    denormalized format
     """
+    print("SafReader.read("+fileLoc+") called")
+
+
+    
