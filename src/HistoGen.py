042bfb3d64445cb33b079edb2c8f5b1126664e90
Effofex <effofex.steem@gmail.com>
2018-06-28 22:42:44 -0400

    Implemented building histograms from df. Does not yet respect command line args for verbosity, output location

--- src/HistoGen.py
+++ src/HistoGen.py
@@ -1,8 +1,59 @@
 import argparse
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
 import SafReader as sr
 
 version = "0.0.1"
 
+# TODO perhaps put this in its own module
+# TODO probably provide more control over file writing
+def build_histograms(df,writeFiles=False,outdir=".",verbose=False,terse=False):
+    """Using a dataframe representing a MadAnalysis5 histogram set as tidy,
+    denormalized table build a collection of matplotlib figures representing
+    those histograms and optionally write them to disk
+
+    Keyword arguments:
+    df -- dataframe containing histogram data
+    writeFiles -- save histograms to disk
+    outdir -- directory in which files should be written
+    verbose --  provide lots of output while working
+    terse -- provide minimal output while working
+    
+    Returns:
+    A collection matplotlib figures
+    """
+    # make life easier by representing relevant columns in df as floats
+    floatCols=['binMin', 'binMax', 'value']
+    df[floatCols] = df[floatCols].apply(pd.to_numeric, errors='coerce')
+    df["bin"]=(safDf.binMin+safDf.binMax)/2
+    
+    #for now, ignore the over/underflow bins - next version will include
+    df = df[df.binMax != np.inf]
+    df = df[df.binMin != -1*np.inf]
+    df["binlabel"]=(df.binMin+df.binMax)/2
+    
+    #generate a unique matplotlib figure for each histogram in the df
+    
+    figno=1
+    
+    #store the figs for returning and more processing
+    figs = []
+    # h is a histogram, identified by unique IDs in the df
+    for h in df.ID.unique() :
+        # these seem like reasonable defaults, they should ideally be
+        # user configurable without having to muck about with th returned figs
+        f = plt.figure(figno, figsize=(6, 4), dpi=80, facecolor='w', edgecolor='k')
+        # since our histo data is already binned, use the weighted version to
+        # reproduce it
+        plt.hist(df[df.ID==h].binlabel,bins=df[df.ID==h].binMin,weights=df[df.ID==h].value)
+        plt.title(df[df.ID==h].name.unique())
+        plt.savefig('Fig_'+str(figno)+'.png', bbox_inches='tight')
+        figs.append((f,df[df.ID==h]))
+        figno=figno+1
+    return(figs)
     
 # Running as a script
 if __name__ == "__main__":
@@ -16,6 +67,8 @@ if __name__ == "__main__":
     parser.add_argument('-v','--version', action='version', version='%(prog)s '+version)
     args = parser.parse_args()
    
+    
     # Parse the file and store as a tidy dataframe
     # probably a much better way to handle verbose/terse flags, but this works
     safDf = sr.read(args.infile,args.verbose,args.terse)
+    build_histograms(safDf)
